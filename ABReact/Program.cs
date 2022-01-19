224050ebbbe61ac36793b4aa1ced062cd214e56e
leneos <shakhov94@yandex.ru>
2022-01-19 13:37:05 +0300

    sasa

--- ABReact/Program.cs
+++ ABReact/Program.cs
@@ -14,60 +14,14 @@ namespace ABReact
 {
     public class Program
     {
-        //public static void Main(string[] args){
-            public static async Task Main(string[] args)
+        public static void Main(string[] args)
+        {
             {
 
                 var host = CreateHostBuilder(args).Build();
-                try
-                {
-                    var scope = host.Services.CreateScope();
 
-                    var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
-                    await ctx.Database.EnsureCreatedAsync();
-
-                    //if (!ctx.Users.Any())
-                    //{
-                    //    for (int i = 0; i < 20; i++)
-                    //    {
-                    //        DateTime created = RandomDay(new DateTime(2010, 1, 1), DateTime.Now);
-                    //        DateTime lastActivity = RandomDay(created, new DateTime(2021, 12, 1));
-                    //        var user = new User
-                    //        {
-                    //            Created = created,
-                    //            LastActivity = lastActivity,
-
-                    //        };
-
-                    //        await ctx.AddAsync(user);
-                    //        await ctx.SaveChangesAsync();
-                    //    }
-
-                    //}
-
-                }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
+                CreateHostBuilder(args).Build().Run();
             }
-    await host.RunAsync();
-    //CreateHostBuilder(args).Build().Run();
-}
-        private static DateTime RandomDay(DateTime start, DateTime end)
-        {
-            int range = 0;
-            Random gen = new Random();
-            
-            if ((end - start).Days != 0)
-            {
-                range = (end - start).Days;
-            }
-            else
-            {
-                range = 7;
-            }
-            return start.AddDays(gen.Next(range));
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
